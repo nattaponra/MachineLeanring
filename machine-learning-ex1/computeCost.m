82a621adca9a0d3bf9a006df5a231187c9f78358
Nattapon Rakthong <nattapon.rakthong@gmail.com>
2018-02-16 00:08:43 +0700

    Change equation

--- machine-learning-ex1/computeCost.m
+++ machine-learning-ex1/computeCost.m
@@ -13,9 +13,10 @@ J = 0;
 % Instructions: Compute the cost of a particular choice of theta
 %               You should set J to the cost.
 
- J =   ((X * theta - y)' * (X * theta - y))/(2*m); % vectorized form
+% J =   ((X * theta - y)' * (X * theta - y))/(2*m); % vectorized form
 
-
+J = 1/(2*m) * (X * theta - y)' * (X * theta - y); % vectorized form
+ 
  
 
 
